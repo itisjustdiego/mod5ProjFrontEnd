0aaf977d37ef9b6fd9975ea7fe56a2a5e936bea4
itisjustdiego <itisjustdiego@gmail.com>
2017-12-20 10:16:38 -0500

    added playerlist

--- src/containers/PlayersList.js
+++ src/containers/PlayersList.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Segment } from 'semantic-ui-react'
 
-const PlayersList = () => {
-    return (
-    <div>
-      hi
-    </div>
-  )
+class PlayersList extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:3001/api/v1/users`)
+    .then(res => res.json())
+    .then(data => this.setState({
+      players:data
+    }))
+  }
+
+  render(){
+    console.log(this.state.players);
+    return(
+      <div>
+        {this.state.players.map(player => <Segment><h2>{player.username}</h2><li>{player.zipcode}</li><li>{player.main_character}</li><li>{player.skill}</li></Segment>)}
+      </div>
+    )
+  }
+
 }
+
 
 export default PlayersList
--- src/containers/UserDashboard.js
+++ src/containers/UserDashboard.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import getCurrentUser from '../actions/auth_actions'
-import bindActionCreators from 'redux'
+// import { connect } from 'react-redux'
+// import getCurrentUser from '../actions/auth_actions'
+// import bindActionCreators from 'redux'
 
 
 const UserDashboard = () => {
